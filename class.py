8926c0521239ef6ac71eee685049bf983d522d78
ganaParakhe <ganeshparakhe0@gmail.com>
2024-10-15 10:15:06 +0530

    1st commit

---
+++ class.py
@@ -0,0 +1,11 @@
+class college:
+    def stud(self,id,name):
+        self.id
+        self.name
+
+    def show(self):
+        print("id=",self.id)
+        print("name",self.name)
+
+obj = college(1,"Ganesh")
+obj.show()
